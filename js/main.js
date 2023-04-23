c7eb80e1c07a0b64d1aed8fdac97af0f3f3acf75
Jana Bartecka <xcrazygirl@seznam.cz>
2023-04-23 20:34:27 +0200

    20230423 ovladani carouselu accesibility

--- js/main.js
+++ js/main.js
@@ -19,3 +19,65 @@ if (currentDay>=0 && currentDay<=4) {
     const innerHTMLdayTomorrow=dayTomorrow.innerHTML
     dayTomorrow.innerHTML=`<span class="regular-event__today">ZÍTRA:</span> ${innerHTMLdayTomorrow}`
 }
+
+// ovladaci panel carouselu pro accesibility
+const glideStop=document.getElementById('glide__stop');
+glideStop.addEventListener('click',StopGlideShow);
+
+function StopGlideShow() {
+    glide.update({
+        autoplay: false
+      }).mount()
+};
+
+const glideStart=document.getElementById('glide__start');
+glideStart.addEventListener('click',StartGlideShow);
+
+function StartGlideShow() {
+    glide.update({
+        autoplay: autoplayValue
+      }).mount()
+};
+
+const MainElement=document.getElementById('main');
+const skipToMain=document.getElementById('glide__skipToMain');
+const glideAccesibility=document.querySelector('.glide__accesibility')
+const mainContent = document.querySelector('main');
+
+skipToMain.addEventListener('focus', (event) => {
+    glideAccesibility.style.opacity='1'
+});
+
+skipToMain.addEventListener('blur', (event) => {
+    glideAccesibility.style.opacity='0'
+  });
+
+glideStart.addEventListener('focus',(event) => {
+    glideAccesibility.style.opacity='1';
+});
+
+glideStart.addEventListener('blur', (event) => {
+    glideAccesibility.style.opacity='0'
+  });
+
+glideStop.addEventListener('focus',(event) => {
+    glideAccesibility.style.opacity='1';
+});
+
+glideStop.addEventListener('blur', (event) => {
+    glideAccesibility.style.opacity='0'
+  });
+  
+skipToMain.addEventListener('click', (event) => {
+    event.preventDefault();
+    mainContent.tabIndex = -1;
+    mainContent.focus();
+  });
+  
+mainContent.addEventListener('blur', () => {
+    mainContent.tabIndex = 0;
+  });
+  
+
+
+
